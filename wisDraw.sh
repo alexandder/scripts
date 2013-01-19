92bee1c9737bfa38ffd73d8c8323ed02f69c69bb
alexandder <aleksanderbolt@yahoo.com>
2013-01-19 10:38:58 -0800

    Create wisDraw.sh

---
+++ wisDraw.sh
@@ -0,0 +1,96 @@
+#!/bin/bash                                                                        
+
+numb=$1
+
+case "$numb" in
+    "1") echo ""
+         echo "|"
+   echo "|"
+	 echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+	 echo "|"
+         echo "|"
+         echo "|"
+         echo "|" ;;
+    "2") echo ""
+         echo "|---------------"
+         echo "|"
+      	 echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|" ;;
+
+      "3") echo ""
+         echo "|---------------"
+         echo "|              ?"
+         echo "|             / \ "
+         echo "|             \ / "
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|"
+         echo "|" ;;
+
+   "4") echo ""
+         echo "|---------------"
+         echo "|              ?"
+         echo "|             / \ "
+         echo "|             \ / "
+         echo "|              |  "
+         echo "|              |  "
+         echo "|              |   "
+         echo "|              |  "
+         echo "|"
+         echo "|"
+         echo "|" ;;
+
+   "5") echo ""
+         echo "|---------------"
+         echo "|              ?"
+         echo "|             / \ "
+         echo "|             \ / "
+         echo "|              |  "
+         echo "|             /|\  "
+         echo "|            / | \  "
+         echo "|              |   "
+         echo "|"
+         echo "|"
+         echo "|" ;;
+
+   "6") echo ""
+         echo "|---------------"
+         echo "|              ?"
+         echo "|             / \ "
+         echo "|             \ / "
+         echo "|              |  "
+         echo "|             /|\  "
+         echo "|            / | \  "
+         echo "|              |   "
+         echo "|             /     "
+         echo "|            /      "
+         echo "|" ;;
+   "7") echo ""
+         echo "|---------------"
+         echo "|              ?"
+         echo "|             / \ "
+         echo "|             \ / "
+         echo "|              |  "
+         echo "|             /|\  "
+         echo "|            / | \  "
+         echo "|              |   "
+         echo "|             / \      "
+         echo "|            /   \     "
+         echo "|" ;;
+
+    *)   echo "Koniec"
+esac
